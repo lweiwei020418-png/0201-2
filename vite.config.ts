91b2084fa222566042a04b50a4a03b5cbce5f1c0
lweiwei020418-png <lweiwei020418@gmail.com>
2026-02-02 02:55:42 +0800

    Update vite.config.ts
    修改代码问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,23 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:63e5a78aeea5894837a62ad0b90869c602bad713f93ae44e61c436a7b7689a19
-size 580
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, '.', '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      define: {
+        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
+      },
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, '.'),
+        }
+      }
+    };
+});
